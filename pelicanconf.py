fe0ca767df7f92530a1e590a99a699b813f4e646
Carlos Leite <carlos.leite@znc.com.br>
2016-01-09 12:31:43 -0200

    add theme settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,16 +21,37 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('ZNC Sistemas', 'http://znc.com.br'),
+    ('PythonPeople', 'http://people.python.org.br/'),
+    ('Python.org', 'http://python.org/'),
+    ('Pelican', 'http://getpelican.com/'),
+    )
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('bitbucket', 'https://bitbucket.org/carlos_leite/'),
+          ('github', 'https://github.com/cadu-leite'),
+          ('g+', 'https://google.com/+CarlosLeite'),
+          ('linkedin', 'https://br.linkedin.com/in/carlosleite/en'),
+          ('rss', '//blog.alexandrevicenzi.com/feeds/all.atom.xml'))
+
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+#CUSTOM Booystrap3 settings
+# the Bootstrap3 theme has benn customized.
+# Settings VARS name didnt, but the behavior slightly diff
+HIDE_SIDEBAR = True
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+SHOW_ARTICLE_AUTHOR = True
+SHOW_ARTICLE_CATEGORY = True
+SHOW_DATE_MODIFIED = True
+
+DISPLAY_BREADCRUMBS = False
+DISPLAY_CATEGORY_IN_BREADCRUMBS = False
